13e61394d4d2aa5028bb3ea9146896e27d94c9ca
Konstantin Schavlev <k.s.v2@mail.ru>
2021-05-24 09:05:44 +0300

    added class plane

---
+++ Airport/plane.cpp
@@ -0,0 +1,13 @@
+#include "plane.h"
+
+int plane::get() {
+	return _memb;
+}
+
+void plane::set(int a) {
+	int _memb = a;
+}
+
+plane::plane()
+{
+}
---
+++ Airport/plane.h
@@ -0,0 +1,10 @@
+#pragma once
+ref class plane
+{
+	int _memb;
+public:
+	plane();
+	int get();
+	void set(int a);
+};
+
